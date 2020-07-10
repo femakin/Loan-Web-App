0c76085acf02c92828aa5b1b1ff19b0ac1fe6ac2
femakin <akinfemi46@gmail.com>
2020-07-10 17:19:25 +0100

    Adjust the js logic

--- loan.js
+++ loan.js
@@ -10,21 +10,25 @@ function eligibility(){
     //let  interest;
     //let dura = parseInt(Duration);
     let Message = document.querySelector("#result");
-    if(amt < 50000 && Duration != ""  && Loan !="" || Loan < 50000){
+    if(Loan < 50000 && Duration != ""  &&  amt < 50000){
         Message.textContent = "Please! Your journey ends here as you're not eligible for the loan";
        // console.log('Try again')
+    }else if(amt > 50000 && Duration != ""  &&  Loan < 50000){
+        Message.textContent = "Please! Your journey ends here as you're not eligible for the loan";
     }
     //Change this else-if to just an else since you are just checking for the exact opposite of your if statement
-    else  if(amt >= 50000  && Duration != ""  &&  Loan !="") {
+    //else  if(Loan >= 50000 && Loan <= 70000 && amt >= 50000 && Duration != ""  &&  Loan !="") 
+    
+     else  if(Loan >= 50000  && Duration != ""  &&  Loan !="") {
         Message.textContent = "Congratulations! Please Check the information below and apply"
       //  console.log('Please Fill the form below and apply')
     }   
     
-  if (Loan >= 50000 && Loan <= 70000){
+  if (Loan >= 50000 && Loan <= 70000  ){
     rate = 7/100;
     interest = rate * Loan;
   }
-  if(Loan >=70001 && Loan <= 90000){
+  if(Loan >=70001 && Loan <= 90000   ){
       rate = 8/100;
       interest = rate * Loan;
   }
